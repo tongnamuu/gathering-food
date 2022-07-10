2683977afafdf10dfbca42f4ccb612f39c6ee2d6
tongnamuu <thban33@naver.com>
2022-07-10 23:15:21 +0900

    feat: Init Project

---
+++ src/main/java/com/tongnamuu/gatheringfood/GatheringFoodApplication.java
@@ -0,0 +1,13 @@
+package com.tongnamuu.gatheringfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tongnamuu/gatheringfood/GatheringFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongnamuu.gatheringfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
