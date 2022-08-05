888953c170111c22cbf78e0ae0add911f808b847
tongnamuu <thban33@naver.com>
2022-08-06 02:00:46 +0900

    feat: Remove Admin

--- src/main/java/com/tongnamuu/gatheringfood/api/user/domain/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tongnamuu.gatheringfood.api.user.domain.repository;
-
-import com.tongnamuu.gatheringfood.api.user.domain.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
